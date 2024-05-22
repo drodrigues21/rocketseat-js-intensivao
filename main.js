09365e279f3f55f790b8a8b6bfe7a21d397cc83e
David Rodrigues <davidvilar21@gmail.com>
2024-05-21 21:16:34 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,80 @@
+// Crie um script que exiba a mensagem "Hello World!" em um alerta no navegador.
+alert("Hello world!");
+
+// Crie um script que declare duas variáveis e exiba o resultado da soma entre elas.
+
+let a = 5;
+let b = 10;
+
+console.log(a + b);
+
+// Crie um script que declare uma variável e verifique se o seu valor é um número. Se for, exiba a mensagem "É um número", caso contrário, exiba a mensagem "Não é um número".
+
+let c = 5;
+
+if (typeof c === "number") {
+   console.log("É um número");
+} else {
+   console.log("Não é um número");
+}
+
+// Crie um script que declare uma variável e verifique se o seu valor é uma string. Se for, exiba a mensagem "É uma string", caso contrário, exiba a mensagem "Não é uma string".
+
+let d = "5";
+
+if (typeof d === "string") {
+   console.log("É uma string");
+} else {
+   console.log("Não é uma string");
+}
+
+// Crie um script que declare uma variável e verifique se o seu valor é um booleano. Se for, exiba a mensagem "É um booleano", caso contrário, exiba a mensagem "Não é um booleano".
+
+let e = true;
+
+if (typeof e === "boolean") {
+   console.log("É um booleano");
+} else {
+   console.log("Não é um booleano");
+}
+
+// Crie um script que declare duas variáveis e exiba o resultado da subtração entre elas.
+
+let f = 5;
+let g = 10;
+
+console.log(f - g);
+
+// Crie um script que declare duas variáveis e exiba o resultado da multiplicação entre elas.
+
+let h = 5;
+let i = 10;
+
+console.log(h * i);
+
+// Crie um script que declare duas variáveis e exiba o resultado da divisão entre elas.
+
+let j = 5;
+let k = 10;
+
+console.log(j / k);
+
+// Crie um script que declare uma variável e verifique se o seu valor é um número par. Se for, exiba a mensagem "É um número par", caso contrário, exiba a mensagem "Não é um número par".
+
+let l = 4;
+
+if (l % 2 === 0) {
+   console.log("É um número par");
+} else {
+   console.log("Não é um número par");
+}
+
+// Crie um script que declare uma variável e verifique se o seu valor é um número ímpar. Se for, exiba a mensagem "É um número ímpar", caso contrário, exiba a mensagem "Não é um número ímpar".
+
+let m = 5;
+
+if (m % 2 !== 0) {
+   console.log("É um número ímpar");
+} else {
+   console.log("Não é um número ímpar");
+}
